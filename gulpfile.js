988ab74e2eff9392d3aab3d7f2f489989484e383
Daniel Ramos <danielramosacosta@hotmail.com>
2016-07-25 12:08:45 +0100

    Movido el build a gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,35 @@
 const gulp = require('gulp')
 const spawn = require('child_process').spawn
 const del = require('del')
-const zip = require('gulp-zip')
+// const zip = require('gulp-zip')
+const path = require('path')
 const standard = require('gulp-standard')
 var mkdirp = require('mkdirp')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+const autoprefixer = require('autoprefixer')
+
+var debug = process.env.NODE_ENV !== 'production'
 
 // Requerirlo para que salte el error si no se hizo npm install
-require('webpack')
+const webpack = require('webpack-stream')
+
 require('webpack-dev-server')
 require('gulp-stats')(gulp)
 
 gulp.task('zip', ['build', 'copyfiles', 'compress', 'cleanBuild'])
 
 gulp.task('build', ['clean'], (cb) => {
-  spawn('node', [
-    './node_modules/.bin/webpack',
-    '-c'
-  ], {stdio: 'inherit'})
-  .on('exit', cb)
+  return gulp.src('app/scripts/client.jsx')
+    .pipe(webpack(webpackConfig))
+    .pipe(gulp.dest('dist/'))
 })
 
 gulp.task('copyfiles', ['build'], () => {
   return gulp.src([
     './addon/*',
     './LICENSE.txt',
-    './README.md',
+    './README.md'
   ])
   .pipe(gulp.dest('./webinterface.mondieu'))
 })
@@ -74,3 +79,78 @@ gulp.task('lint', function () {
       breakOnError: false
     }))
 })
+
+
+// ============================================================================
+// Webpack Config
+// ============================================================================
+
+const sassLoaders = [
+  'css-loader',
+  'postcss-loader',
+  'sass-loader?indentedSyntax=sass&includePaths[]=' + path.resolve(__dirname, './app')
+].join('!')
+
+let commonPlugins = [
+  new ExtractTextPlugin('main.css'),
+  new HtmlWebpackPlugin({template: 'index.html'})
+]
+
+const webpackConfig = {
+  context: path.join(__dirname, '/app'),
+  devtool: debug ? 'inline-sourcemap' : null,
+  module: {
+    loaders: [
+      {
+        test: /\.js[x]?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
+        }
+      },
+      {
+        test: /\.sass$/,
+        loader: ExtractTextPlugin.extract('style-loader', sassLoaders)
+      },
+      {
+        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.png$/,
+        loader: 'url-loader?limit=100000'
+      },
+      {
+        test: /\.jpg$/,
+        loader: 'file-loader'
+      }
+    ]
+  },
+  output: {
+    filename: 'main.js'
+  },
+  plugins: debug ? [].concat(commonPlugins) : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })
+  ].concat(commonPlugins),
+  postcss: [
+    autoprefixer({
+      browsers: ['last 2 versions']
+    })
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.sass'],
+    root: [path.join(__dirname, './app')]
+  }
+}
