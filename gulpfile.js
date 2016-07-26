bf9990fc7ec5ab1cc6986172d4bf07a818669b2d
Daniel Ramos <danielramosacosta@hotmail.com>
2016-07-26 08:58:55 +0100

    Cambiadas las tareas y añadidas al npm

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,70 @@
-const gulp = require('gulp')
-const del = require('del')
-const path = require('path')
-const standard = require('gulp-standard')
-const mkdirp = require('mkdirp')
-const gutil = require('gulp-util')
-const zipFolder = require('zip-folder')
-const webpackStream = require('webpack-stream')
-const webpack = require('webpack')
-const WebpackDevServer = require('webpack-dev-server')
+const gulp             = require('gulp') // eslint-disable-line
+const del              = require('del') // eslint-disable-line
+const path             = require('path') // eslint-disable-line
+const standard         = require('gulp-standard') // eslint-disable-line
+const mkdirp           = require('mkdirp') // eslint-disable-line
+const gutil            = require('gulp-util') // eslint-disable-line
+const zipFolder        = require('zip-folder') // eslint-disable-line
+const runSequence      = require('run-sequence') // eslint-disable-line
+const webpack          = require('webpack') // eslint-disable-line
+const webpackStream    = require('webpack-stream') // eslint-disable-line
+const WebpackDevServer = require('webpack-dev-server') // eslint-disable-line
 
 // Activar estadísticas de las tareas
 require('gulp-stats')(gulp)
 
-const debug = process.env.NODE_ENV !== 'production'
-
-const addonName = 'webinterface.mondieu'
-
-gulp.task('zip', ['build', 'copyfiles', 'compress', 'cleanBuild'])
+global.tasksConfig = {
+  debug: false,
+  addonName: 'webinterface.mondieu',
+  destDir: 'dist'
+}
 
 gulp.task('build', ['clean'], (cb) => {
+  global.tasksConfig.debug = false
   return gulp.src('app/scripts/client.jsx')
     .pipe(webpackStream(webpackConfig))
-    .pipe(gulp.dest(`${addonName}/${addonName}`))
+    .pipe(gulp.dest(global.tasksConfig.destDir))
 })
 
-gulp.task('copyfiles', ['build'], () => {
+// Zip tasks
+
+gulp.task('release', (cb) => {
+  global.tasksConfig.destDir = `${global.tasksConfig.addonName}/${global.tasksConfig.addonName}`
+  runSequence(['build', 'copyfiles'], 'compress', 'cleanBuild', cb)
+})
+
+gulp.task('copyfiles', () => {
   return gulp.src([
     './addon/*',
     './LICENSE.txt',
     './README.md'
   ])
-  .pipe(gulp.dest(`${addonName}/${addonName}`))
+  .pipe(gulp.dest(global.tasksConfig.destDir))
 })
 
-gulp.task('compress', ['copyfiles'], (cb) => {
+gulp.task('compress', (cb) => {
   mkdirp('./dist', () => {
-    zipFolder(path.join(__dirname, addonName), path.join(__dirname, `/dist/${addonName}.zip`), (err) => {
-      if (err) {
-        gutil.log('[zipFolder]', err)
-      } else {
-        gutil.log('[zipFolder]', 'Done')
-      }
-      cb()
-    })
+    zipFolder(
+      path.join(__dirname, global.tasksConfig.addonName),
+      path.join(__dirname, `/dist/${global.tasksConfig.addonName}.zip`), (err) => {
+        if (err) {
+          gutil.log('[zipFolder]', err)
+        } else {
+          gutil.log('[zipFolder]', 'Done')
+        }
+        cb()
+      })
   })
 })
 
-gulp.task('cleanBuild', ['compress'], (cb) => {
-  return del([addonName], cb)
+gulp.task('cleanBuild', (cb) => {
+  return del([global.tasksConfig.addonName], cb)
 })
 
-gulp.task('serve', ['clean'], (cb) => {
+// End Zip tasks
+
+gulp.task('dev', (cb) => {
+  global.tasksConfig.debug = true
   let ip = process.env.IP || '0.0.0.0'
 
   new WebpackDevServer(webpack(webpackConfig), {
@@ -100,7 +114,7 @@ let commonPlugins = [
 
 const webpackConfig = {
   context: path.join(__dirname, '/app'),
-  devtool: debug ? 'inline-sourcemap' : null,
+  devtool: global.tasksConfig.debug ? 'inline-sourcemap' : null,
   entry: './scripts/client.jsx',
   module: {
     loaders: [
@@ -140,14 +154,21 @@ const webpackConfig = {
     ]
   },
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, global.tasksConfig.destDir),
     filename: 'main.js'
   },
-  plugins: debug ? [].concat(commonPlugins) : [
+  plugins: global.tasksConfig.debug ? commonPlugins : commonPlugins.concat([
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })
-  ].concat(commonPlugins),
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: false,
+      sourcemap: false,
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    })
+  ]),
   postcss: [
     autoprefixer({
       browsers: ['last 2 versions']
